8bbbc0cf91bceb48c56c34734bbc38a6bdd4ed78
Etienne <eschussl@student.42.fr>
2024-12-06 16:07:49 +0100

    push channel.hpp

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eschussl <eschussl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 15:23:07 by aduvilla          #+#    #+#             */
-/*   Updated: 2024/12/05 15:24:56 by aduvilla         ###   ########.fr       */
+/*   Updated: 2024/12/06 16:06:09 by eschussl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,33 @@
 # define CHANNEL_HPP
 
 #include <string>
+#include <vector>
+#include "Client.hpp"
 
 class	Channel
 {
 	public:
-		Channel(void);
+		Channel(const std::string &name, const std::string &passwd);
+		Channel(const std::string &name);
 		Channel(Channel const & src);
 		Channel &operator=(Channel const & rhs);
 		~Channel(void);
+		void addClient(Client&);
+		void addClient(Client&, const std::string&passwd);
+		void removeClient(const Client&);
+		void sendAllMsg(const std::string &);
+		void setName(const std::string &);
+		void setTopic(const std::string &);
+		const std::string getName() const;
+		const std::string getTopic() const;
+		
 
 	private:
+		bool 		invite_only;
+		std::string m_topic;
 		std::string	m_name;
 		std::string	m_password;
+		std::vector <Client&> m_clients;
 };
 
 #endif
